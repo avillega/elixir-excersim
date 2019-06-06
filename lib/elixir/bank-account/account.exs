ee2deaaced27821e16e00a131c1ef1fc4a1d91bd
avillega <villegas.pelaez.andres@gmail.com>
2019-06-05 22:25:46 -0500

    Add solution to bank_account

---
+++ lib/elixir/bank-account/account.exs
@@ -0,0 +1,54 @@
+defmodule BankAccount do
+  use Agent
+
+  @moduledoc """
+  A bank account that supports access from multiple processes.
+  """
+
+  @typedoc """
+  An account handle.
+  """
+  @opaque account :: pid
+
+  @doc """
+  Open the bank. Makes the account available.
+  """
+  @spec open_bank() :: account
+  def open_bank() do
+    {:ok, pid} = Agent.start_link(fn -> 0 end)
+    pid
+  end
+
+  @doc """
+  Close the bank. Makes the account unavailable.
+  """
+  @spec close_bank(account) :: none
+  def close_bank(account) do
+    case Process.alive?(account) do
+      true -> Agent.stop(account)
+      _ -> {:error, :account_closed}
+    end
+  end
+
+  @doc """
+  Get the account's balance.
+  """
+  @spec balance(account) :: integer
+  def balance(account) do
+    case Process.alive?(account) do
+      true -> Agent.get(account, & &1)
+      _ -> {:error, :account_closed}
+    end
+  end
+
+  @doc """
+  Update the account's balance by adding the given amount which may be negative.
+  """
+  @spec update(account, integer) :: any
+  def update(account, amount) do
+    case Process.alive?(account) do
+      true -> Agent.update(account, &(&1 + amount))
+      _ -> {:error, :account_closed}
+    end
+  end
+end
